45f3407376549a42bed27e48b612f3c3cdd4fc8d
omark807 <omark807@gmail.com>
2022-08-25 11:42:59 -0500

    Removing unused imports

--- pages/works/kayak.js
+++ pages/works/kayak.js
@@ -1,9 +1,8 @@
-import { Box, Container, Badge, Image, Link, List, ListItem, OrderedList, UnorderedList, Heading } from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem, OrderedList, UnorderedList, Heading } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
-import Head from 'next/head'
 
 const Project = () => (
     <Layout>

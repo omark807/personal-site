b1b32961768c2606c52461cb9adb0c2779ecad51
Omar Khan <omark807@gmail.com>
2024-08-06 17:50:59 -0500

    Removing unused import

--- pages/research.js
+++ pages/research.js
@@ -1,4 +1,4 @@
-import { Container, Heading, Link,  Text, Divider} from '@chakra-ui/react'
+import { Container, Heading, Link, Divider} from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 
@@ -23,7 +23,7 @@ const Works = () => (
                 </Heading> 
                 <Paragraph>
                     <b>[C1]</b> Chiang, Y.S, Khan, O., Bates, A., and Cobb, C. (2024). 
-                    <b>More than just informed: The importance of consent facets in smart homes.</b> 
+                    <i>More than just informed: The importance of consent facets in smart homes.</i> 
                     In Proceedings of the CHI Conference on Human Factors in Computing Systems (CHI ’24), May 11–16, 2024, Honolulu, HI, USA. ACM, New
                     York, NY, USA, 21 pages
                     <Link target="_blank" href='https://dl.acm.org/doi/10.1145/3613904.3642288'> [Link]</Link> 
@@ -31,8 +31,7 @@ const Works = () => (
                 <Divider my={3} />
                 <Paragraph>
                     <b>[C2]</b> Mahmood, S., Zhao, M., Khan, O., Herman, G.L. (2020). 
-                    <b>Caches as an Example of 
-                    Machine-gradable Exam Questions for Complex Engineering Systems.</b> 
+                    <i>Caches as an Example of Machine-gradable Exam Questions for Complex Engineering Systems.</i> 
                     In Proceedings of the 49th ASEE/IEEE Frontiers in Education Conference, Uppsala, Sweden, Oct. 21-24.
                     <Link target="_blank" href='https://doi.org/10.1109/FIE44824.2020.9273822'> [Link]</Link> 
                 </Paragraph>    
@@ -47,7 +46,7 @@ const Works = () => (
                 </Heading>
                 <Paragraph>
                     <b>[P1]</b> Deep, H., Huang, J., Dembi, R., Khan, O., Angrave, L.. 
-                    <b>Webcam Eye-Tracking Based Accessibility for ClassTranscribe.</b>
+                    <i>Webcam Eye-Tracking Based Accessibility for ClassTranscribe.</i>
                     Virtual poster presentation: Illinois Undergraduate Research Symposium 2021
                 </Paragraph> 
             </Section>

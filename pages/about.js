f8c9be523301b503c1c5d4db24bdecf286802bb4
Omar Khan <omark807@gmail.com>
2024-11-14 11:03:33 -0600

    chore: fixing typo in about page

--- pages/about.js
+++ pages/about.js
@@ -32,7 +32,7 @@ const AboutMe = () => (
 
                     <br></br>
 
-                    <Paragraph>And there&apos;s so many ways this has akready been done, and continues to be done as you read this/as we speak/as we exist! 
+                    <Paragraph>And there&apos;s so many ways this has already been done, and continues to be done as you read this/as we speak/as we exist! 
                             As for what I find especially interesting, I find myself thinking a <i>lot</i> about gaps in mental health access and support throughout the world, 
                             specifically for underrepresented communities. While technology rapidly evovles to offer new strategies to manage our health&apos;s multi-faceted nature, 
                             recurring setbacks remain in <b>accessibility</b> and <b>inclusivity</b>. My research investigates and aims to address these gaps by: 

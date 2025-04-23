64a26c10be13cd956c77855aa583af5bf232863b
Omar Khan <omark807@gmail.com>
2025-04-23 13:35:38 -0500

    Adding news page temlpate

---
+++ pages/news.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const News = () => {
+    return (
+        <Layout>
+            <h1>News and Updates</h1>
+            {/* Add news content here */}
+            <p>Check back for the latest updates!</p>
+        </Layout>
+    );
+};
+
+export default News;

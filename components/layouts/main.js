ecf9b316d14e78cdb93d106328660889aa16e8a9
Omar Khan <omark807@gmail.com>
2025-08-11 08:59:53 -0500

    Adding copyright

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Navbar from '../navbar.js'
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Text, useColorModeValue } from '@chakra-ui/react'
 
 const Main = ({ children, router }) => {
+    const footerColor = useColorModeValue('gray.500', 'gray.400')
+    
     return (
         <Box as="main" pb={8}>
             <Head>
@@ -14,6 +16,13 @@ const Main = ({ children, router }) => {
 
             <Container maxW="container.md" pt={14}>
                 {children}
+                
+                {/* Subtle Copyright Footer */}
+                <Box mt={16} mb={4} textAlign="center">
+                    <Text fontSize="sm" color={footerColor}>
+                        © {new Date().getFullYear()} Omar Khan. All rights reserved.
+                    </Text>
+                </Box>
             </Container>
         </Box>
     )
